a031387bb71d01838f0e04220ec730a1e2771401
Madelyn Spalding <mspaldingworks@gmail.com>
2022-05-27 14:55:03 -0400

    I've got a framework going, but it currently isn't running with an unknown error not listed in the AS project errors.

---
+++ app/src/main/java/com/mad/e/recipeapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.mad.e.recipeapp.ui.home
+
+
+import androidx.lifecycle.*
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
